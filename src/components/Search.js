c4a14ea0289afef17c5572542ac15e99e536401a
TEST2-jpg <agapdag@gmail.com>
2022-07-28 19:58:38 +0800

    Add uls

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,24 @@
-import { useState} from 'react'
+import { useState } from 'react'
 import Description from "./Description"
-const Search = ({data}) => {
+
+const Search = ({ data }) => {
     const [term, setTerm] = useState('')
     const [searchedName, setsearchedName] = useState('')
     const searchFood = () => {
         const name = data.filter(food => {
-            if(!food.Display_Name) return null
-            return food.Display_Name.toLowerCase().includes(term.toLowerCase())
+            if (!food.Display_Name) return null
+            return food.Display_Name.toLowerCase().includes(" "+term.toLowerCase()) || food.Display_Name.toLowerCase().includes(term + " ".toLowerCase())
         })
         setsearchedName(name)
     }
-    
+
     return (
         <div>
-            <input type='text' value={term} onChange={e => setTerm(e.target.value)}/>
+            <input type='text' value={term} onChange={e => setTerm(e.target.value)} />
             <span><button onClick={searchFood}>Submit</button></span>
-            {searchedName && searchedName.slice(0, 15).map((info, i) => <Description info={info} i={i}/>)}
+            <ul class="list-group list-group-flush">
+                {searchedName && searchedName.slice(0, 25).map((info, i) => <Description info={info} i={i} />)}
+            </ul>
         </div>
     )
 }
