178f72c4d2bde1bd7bde73123671e2ea91f5915c
TEST2-jpg <agapdag@gmail.com>
2022-07-29 13:48:28 +0800

    Add load margin

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,7 +11,7 @@ const Search = ({ data }) => {
         if (term === '') return setsearchedName(false)
         const name = data.filter(food => {
             if (!food.Display_Name) return null
-            return food.Display_Name.toLowerCase().includes(" " + term.toLowerCase()) || food.Display_Name.toLowerCase().includes(term + " ".toLowerCase())
+            return food.Display_Name.toLowerCase().includes(term.toLowerCase())
         })
         setsearchedName(name)
     }
@@ -26,18 +26,18 @@ const Search = ({ data }) => {
     }
     
     return (
-        <div className='search container'>
+        <div className='search container rounded'>
             <div className='input-group container p-3 border rounded mwidth sticky-top'>
                 <input type='text' placeholder='e.g., pineapple, oreo, etc.' className='form-control rounded shadow-none bg-success bg-opacity-10' value={term} onChange={e => setTerm(e.target.value)} />
                 <span><button className='btn btn-light mx-1 shadow-none' onClick={searchFood}>SEARCH</button></span>
                 <span><button className='btn btn-light mx-1 shadow-none' onClick={clear}>CLEAR ALL</button></span>
             </div>
             {searchedName ? <p className='text-center mt-3'>{searchedName.length} results found</p> : null}
-            {searchedName ? <ul className="list-group list-group-flush">
+            {searchedName ? <ul className="list-group list-group-flush rounded">
                 {!searchedName.length ? <p>No matches found</p> : searchedName.slice(0, itemNum).map((info, i) => <Description info={info} i={i} key={i} />)}
             </ul> : <p className='text-center mt-3'>Search a food</p>}
             { itemNum <= searchedName.length  && <div className='text-center'>
-                <button className='btn btn-primary text-center' onClick={loadMore}>Load</button>
+                <button className='btn btn-primary text-center mt-3' onClick={loadMore}>Load</button>
             </div>}
         </div>
     )
