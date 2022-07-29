db4abee1ce1932e1bd01bc45a92749b791aecacf
TEST2-jpg <agapdag@gmail.com>
2022-07-29 13:32:04 +0800

    Improved ui

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,6 +7,7 @@ const Search = ({ data }) => {
     const [itemNum, setItemNum] = useState(25)
     const searchFood = () => {
         setsearchedName([])
+        setItemNum(25)
         if (term === '') return setsearchedName(false)
         const name = data.filter(food => {
             if (!food.Display_Name) return null
@@ -17,22 +18,27 @@ const Search = ({ data }) => {
     const clear = () => {
         setsearchedName(false)
         setTerm('')
+        setItemNum(25)
     }
 
     const loadMore = () => {
         setItemNum(items => items + 25)
     }
+    
     return (
-        <div>
-            <div className='input-group container p-3 border rounded mwidth'>
-                <input type='text' className='form-control rounded shadow-none bg-success bg-opacity-10' value={term} onChange={e => setTerm(e.target.value)} />
-                <span><button className='btn btn-light mx-1' onClick={searchFood}>SEARCH</button></span>
-                <span><button className='btn btn-light mx-1' onClick={clear}>CLEAR ALL</button></span>
+        <div className='search container'>
+            <div className='input-group container p-3 border rounded mwidth sticky-top'>
+                <input type='text' placeholder='e.g., pineapple, oreo, etc.' className='form-control rounded shadow-none bg-success bg-opacity-10' value={term} onChange={e => setTerm(e.target.value)} />
+                <span><button className='btn btn-light mx-1 shadow-none' onClick={searchFood}>SEARCH</button></span>
+                <span><button className='btn btn-light mx-1 shadow-none' onClick={clear}>CLEAR ALL</button></span>
             </div>
+            {searchedName ? <p className='text-center mt-3'>{searchedName.length} results found</p> : null}
             {searchedName ? <ul className="list-group list-group-flush">
                 {!searchedName.length ? <p>No matches found</p> : searchedName.slice(0, itemNum).map((info, i) => <Description info={info} i={i} key={i} />)}
-            </ul> : <p>Search a food</p>}
-            <button onClick={loadMore}>Load</button>
+            </ul> : <p className='text-center mt-3'>Search a food</p>}
+            { itemNum <= searchedName.length  && <div className='text-center'>
+                <button className='btn btn-primary text-center' onClick={loadMore}>Load</button>
+            </div>}
         </div>
     )
 }
